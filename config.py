0c70861e8410bebdeb1a159397db395002f0b20b
John Sanchez <jostsace05@gmail.com>
2026-06-05 18:07:54 -0600

    base model version

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 def get_config():
     return {
         "batch_size": 8,
-        "num_epochs": 10,
+        "num_epochs": 15,
         "lr": 10**-4,
         "seq_len": 128,
         "d_model": 512,
@@ -12,10 +12,19 @@ def get_config():
         "lang_tgt": "es",
         "model_folder": "models/en_es_v8",
         "model_basename": "en_es_v8_",
-        "preload": "05",
+        "preload": "14",
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/en_es_v8_"
     }
+
+"""
+Modelo version 8: en_es_v8
+Epoch 10 - Loss 4.366
+Epoch 11 - Loss 3.987
+Epoch 12 - Loss 3.550
+Epoch 13 - Loss 4.417
+Epoch 14 - Loss 4.072
+"""
 
 def get_weights_file_path(config, epoch: str):
     model_folder = config['model_folder']
